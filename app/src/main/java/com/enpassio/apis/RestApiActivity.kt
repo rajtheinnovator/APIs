45cd897c680d16f04305ef54fef7a839a003a969
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2018-10-05 23:04:03 +0530

    Refactored code

--- app/src/main/java/com/enpassio/apis/RestApiActivity.kt
+++ app/src/main/java/com/enpassio/apis/RestApiActivity.kt
@@ -2,117 +2,57 @@ package com.enpassio.apis
 
 
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.BitmapFactory
 import android.media.MediaPlayer
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Bundle
-import android.support.annotation.Nullable
 import android.support.customtabs.CustomTabsIntent
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.Scope
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import saschpe.android.customtabs.CustomTabsHelper
 import saschpe.android.customtabs.WebViewFallback
-import java.util.*
 
 class RestApiActivity : AppCompatActivity() {
 
 
     private val TAG = RestApiActivity::class.java.simpleName
-    private val RC_GET_TOKEN = 9002
-
-    private var mGoogleSignInClient: GoogleSignInClient? = null
     lateinit var mIdTokenTextView: TextView
-    lateinit var mRefreshButton: Button
-
-    // Button click listeners
-    lateinit var signIn: Button
-    lateinit var signOut: Button
-    lateinit var disconnect: Button
     lateinit var restApiPlayground: Button
     // Scope for reading user's contacts
     private val CONTACTS_SCOPE = "https://mail.google.com/+https://www.googleapis.com/auth/userinfo.email"
     private val redirectUri = BuildConfig.REDIRECT_URI
-
     var player: MediaPlayer? = null
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rest_api)
-
-        mRefreshButton = findViewById(R.id.button_refresh_id)
         mIdTokenTextView = findViewById(R.id.text_view_value_token)
-
-
-        // Button click listeners
-        signIn = findViewById(R.id.button_sign_in)
-        signIn.setOnClickListener { startSignIn() }
-        signOut = findViewById(R.id.button_sign_out)
-        signOut.setOnClickListener { startSignOut() }
-        disconnect = findViewById(R.id.button_disconnect)
-        disconnect.setOnClickListener { startDisconnection() }
-        mRefreshButton.setOnClickListener { refresh() }
         restApiPlayground = findViewById(R.id.button_rest_api_playground)
         restApiPlayground.setOnClickListener { checkIfTokenIsAvailable() }
-
-
         val alarmButton: Button = findViewById(R.id.button_ring_alarm)
         val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
         player = MediaPlayer.create(this@RestApiActivity, notification)
         alarmButton.setOnClickListener {
             ringAlarm()
         }
-        //checkIfTokenIsAvailable()
-
-        // [START configure_signin]
-        // Request only the user's ID token, which can be used to identify the
-        // user securely to your backend. This will contain the user's basic
-        // profile (name, profile picture URL, etc) so you should not need to
-        // make an additional call to personalize your application.
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(BuildConfig.GOOGLE_API_CLIENT_ID)
-                .requestEmail()
-                .requestScopes(Scope(CONTACTS_SCOPE))
-                .build()
-        // [END configure_signin]
-
-        // Build GoogleAPIClient with the Google Sign-In API and the above options.
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     //check if token exist already or not and act accordingly
     private fun checkIfTokenIsAvailable() {
-//        val sharedPref = getPreferences(Context.MODE_PRIVATE)
-//        val token = sharedPref.getString("token", "")!!
-
         val settings = getSharedPreferences("token", Context.MODE_PRIVATE)
         val token = settings.getString("token", "")!!
-
         if (token.isEmpty())
             setupTokenWithRestApi()
         else
             getAccessTokenFromRefreshToken(token)
         Log.v("my_tag", "token inside restApiActivity is: " + token)
     }
-
 
     private fun ringAlarm() {
         if (player?.isPlaying!!) {
@@ -134,10 +74,8 @@ class RestApiActivity : AppCompatActivity() {
                 .setShowTitle(true)
                 .setCloseButtonIcon(icon)
                 .build()
-
         // This is optional but recommended
         CustomTabsHelper.addKeepAliveExtra(this, customTabsIntent.intent);
-
         // This is where the magic happens...
         CustomTabsHelper.openCustomTab(this, customTabsIntent,
                 Uri.parse("https://accounts.google.com/o/oauth2/v2/auth?prompt=consent"
@@ -147,7 +85,6 @@ class RestApiActivity : AppCompatActivity() {
                         + "&access_type=offline&Content-Type=application/json"
                         + "&redirect_uri=" + redirectUri),
                 WebViewFallback());
-
     }
 
 
@@ -192,151 +129,26 @@ class RestApiActivity : AppCompatActivity() {
 
     private fun saveTokenToSharedPreference(accessTokenResponse: Response<AccessToken>) {
         // save token
-//        val sharedPref = getSharedPreferences("token", MODE_PRIVATE)
-//        with(sharedPref.edit()) {
-//            putString("token", accessTokenResponse.body()?.accessToken)
-//            apply()
-//        }
-
         val settings: SharedPreferences = getSharedPreferences("token", Context.MODE_PRIVATE);
         val editor: SharedPreferences.Editor = settings.edit();
         editor.putString("token", accessTokenResponse.body()?.accessToken);
-        editor.commit();
+        editor.commit()
+        mIdTokenTextView.text = accessTokenResponse.body()?.accessToken
     }
 
 
     private fun getAccessTokenFromRefreshToken(refreshToken: String) {
         val refreshTokenService = APIClient.client.create(RefreshTokenService::class.java)
-
         val call =
                 refreshTokenService.getAccessTokenFromRfreshToken(
                         refreshToken,
                         BuildConfig.GOOGLE_API_CLIENT_ID,
                         "refresh_token")
         call.enqueue(object : Callback<AccessToken> {
-            override fun onFailure(call: Call<AccessToken>, t: Throwable) {
-
-            }
-
+            override fun onFailure(call: Call<AccessToken>, t: Throwable) {}
             override fun onResponse(call: Call<AccessToken>, response: Response<AccessToken>) {
                 Log.v("my_tag", "token from refresh token is :" + response.body()?.accessToken)
             }
         })
-    }
-
-    // [START handle_sign_in_result]
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-            // TODO(developer): send ID Token to server and validate
-
-            updateUI(account)
-        } catch (e: ApiException) {
-            Log.e(TAG, "handleSignInResult:error", e)
-            updateUI(null)
-        }
-
-    }
-
-    // [END handle_sign_in_result]
-    private fun startDisconnection() {
-        mGoogleSignInClient!!.revokeAccess().addOnCompleteListener(this,
-                object : OnCompleteListener<Void> {
-                    override fun onComplete(task: Task<Void>) {
-                        updateUI(null)
-                    }
-                })
-    }
-
-    private fun startSignOut() {
-        mGoogleSignInClient!!.revokeAccess().addOnCompleteListener(this,
-                object : OnCompleteListener<Void> {
-                    override fun onComplete(task: Task<Void>) {
-                        updateUI(null)
-                    }
-                })
-    }
-
-    private fun startSignIn() {
-        // Show an account picker to let the user choose a Google account from the device.
-        // If the GoogleSignInOptions only asks for IDToken and/or profile and/or email then no
-        // consent screen will be shown here.
-        val signInIntent = mGoogleSignInClient?.getSignInIntent()
-        startActivityForResult(signInIntent, RC_GET_TOKEN)
-    }
-
-
-    private fun refresh() {
-        // Attempt to silently refresh the GoogleSignInAccount. If the GoogleSignInAccount
-        // already has a valid token this method may complete immediately.
-        //
-        // If the user has not previously signed in on this device or the sign-in has expired,
-        // this asynchronous branch will attempt to sign in the user silently and get a valid
-        // ID token. Cross-device single sign on will occur in this branch.
-        mGoogleSignInClient?.silentSignIn()
-                ?.addOnCompleteListener(this) { task -> handleSignInResult(task) }
-                ?.addOnCompleteListener(this) { task -> handleSignInResult(task) }
-
-    }
-
-    private fun updateUI(@Nullable account: GoogleSignInAccount?) {
-        if (account != null) {
-            val idToken = account.idToken
-            mIdTokenTextView.text = idToken
-
-            signIn.visibility = View.GONE
-            signOut.visibility = View.VISIBLE
-            mRefreshButton.visibility = View.VISIBLE
-
-            fetchUsersData(account)
-        } else {
-            mIdTokenTextView.text = "null"
-
-
-            signIn.visibility = View.VISIBLE
-            signOut.visibility = View.GONE
-            mRefreshButton.visibility = View.GONE
-        }
-    }
-
-    private fun fetchUsersData(account: GoogleSignInAccount) {
-
-        val credential = GoogleAccountCredential.usingOAuth2(
-                this@RestApiActivity,
-                Collections.singleton(CONTACTS_SCOPE))
-        credential.selectedAccount = account.account
-
-
-        val thread = Thread() {
-            kotlin.run {
-                val loginService = ServiceGenerator.createService(GmailService::class.java, credential.token)
-                val call = loginService.getListOfEmails(account.email!!, "", "", "")
-                call.enqueue(object : Callback<ListOfMailIds> {
-                    override fun onResponse(call: Call<ListOfMailIds>, response: Response<ListOfMailIds>) {
-                        val listOfIdsOfMails = response.body()
-                        Log.v("my_tag", "data received is: " + listOfIdsOfMails)
-
-                    }
-
-                    override fun onFailure(call: Call<ListOfMailIds>, t: Throwable) {
-                        Log.e("my_tag", "error is: " + t.message)
-                    }
-                })
-            }
-        }
-        thread.start()
-    }
-
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == RC_GET_TOKEN) {
-            // [START get_id_token]
-            // This task is always completed immediately, there is no need to attach an
-            // asynchronous listener.
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
-            // [END get_id_token]
-        }
     }
 }
