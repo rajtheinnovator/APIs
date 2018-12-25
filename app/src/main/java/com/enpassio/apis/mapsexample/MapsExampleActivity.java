a5eb05d2815ff47f2d7ea2f8377215caf429c33e
enpassio <abhishekraj@abhishekraj.me>
2018-12-25 21:59:26 +0530

    Gps turned on programmatically

--- app/src/main/java/com/enpassio/apis/mapsexample/MapsExampleActivity.java
+++ app/src/main/java/com/enpassio/apis/mapsexample/MapsExampleActivity.java
@@ -1,28 +1,55 @@
 package com.enpassio.apis.mapsexample;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 
 import com.enpassio.apis.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsExampleActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1101;
+public class MapsExampleActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private GoogleMap mMap;
     private Location location;
+
+
+    public static final int LOCATION_PERMISSION_CODE = 2;
+    private static final int LOCATION_SETTINGS_REQUEST_CODE = 1133;
+    //handle gps
+    private GoogleApiClient googleApiClient;
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingsClient;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationCallback mLocationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +58,23 @@ public class MapsExampleActivity extends FragmentActivity implements OnMapReadyC
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //initialize api client
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mSettingsClient = LocationServices.getSettingsClient(this);
+        permissionHandle();
+    }
+
+    private void permissionHandle() {
         int DeviceSdkVersion = Build.VERSION.SDK_INT;
         if (DeviceSdkVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
             if (checkIfLocationPermissionGranted()) {
-                if (turnOnGps()) {
-                    getCurrentLocationOfUser();
-                }
+                enableGps();
+                getCurrentLocationOfUser();
             } else {
                 getUsersLocationPermission();
             }
@@ -46,7 +84,7 @@ public class MapsExampleActivity extends FragmentActivity implements OnMapReadyC
     private void getUsersLocationPermission() {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                LOCATION_PERMISSION_REQUEST_CODE);
+                LOCATION_PERMISSION_CODE);
 
     }
 
@@ -54,8 +92,40 @@ public class MapsExampleActivity extends FragmentActivity implements OnMapReadyC
 
     }
 
-    private boolean turnOnGps() {
-        return false;
+    private void enableGps() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY));
+
+        Task<LocationSettingsResponse> task = LocationServices.getSettingsClient(this).checkLocationSettings(builder.build());
+        task.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                } catch (ApiException e) {
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                            try {
+                                resolvableApiException.startResolutionForResult(MapsExampleActivity.this, LOCATION_SETTINGS_REQUEST_CODE);
+                            } catch (IntentSender.SendIntentException e1) {
+                                e1.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //open setting and switch on GPS manually
+                            break;
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean gpsIsEnabled() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
     }
 
     private boolean checkIfLocationPermissionGranted() {
@@ -66,12 +136,11 @@ public class MapsExampleActivity extends FragmentActivity implements OnMapReadyC
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
-            case LOCATION_PERMISSION_REQUEST_CODE:
+            case LOCATION_PERMISSION_CODE:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //granted
-                    if (turnOnGps()) {
-                        getCurrentLocationOfUser();
-                    }
+                    enableGps();
+                    getCurrentLocationOfUser();
                 } else {
                     //not granted
                     getCoarseLocation();
@@ -94,5 +163,20 @@ public class MapsExampleActivity extends FragmentActivity implements OnMapReadyC
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
