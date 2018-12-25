113a8efcba17ab94999c0bdb3e168fcd0ecf9d26
enpassio <abhishekraj@abhishekraj.me>
2018-12-25 20:08:29 +0530

    Added location permission check

--- app/src/main/java/com/enpassio/apis/mapsexample/MapsExampleActivity.java
+++ app/src/main/java/com/enpassio/apis/mapsexample/MapsExampleActivity.java
@@ -1,7 +1,14 @@
 package com.enpassio.apis.mapsexample;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 
 import com.enpassio.apis.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,7 +20,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsExampleActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1101;
     private GoogleMap mMap;
+    private Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,59 @@ public class MapsExampleActivity extends FragmentActivity implements OnMapReadyC
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        int DeviceSdkVersion = Build.VERSION.SDK_INT;
+        if (DeviceSdkVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (checkIfLocationPermissionGranted()) {
+                if (turnOnGps()) {
+                    getCurrentLocationOfUser();
+                }
+            } else {
+                getUsersLocationPermission();
+            }
+        }
+    }
+
+    private void getUsersLocationPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                LOCATION_PERMISSION_REQUEST_CODE);
+
+    }
+
+    private void getCurrentLocationOfUser() {
+
+    }
+
+    private boolean turnOnGps() {
+        return false;
+    }
+
+    private boolean checkIfLocationPermissionGranted() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //granted
+                    if (turnOnGps()) {
+                        getCurrentLocationOfUser();
+                    }
+                } else {
+                    //not granted
+                    getCoarseLocation();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void getCoarseLocation() {
+
     }
 
     @Override
